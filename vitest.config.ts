0ca706f5c66321af09606ec6b7512581ef431974
marioalvaro <marioalvaro@Marios-MacBook-Pro.local>
2025-03-23 13:03:25 +0800

    Setup Unit Test

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    coverage: {
+      provider: 'v8', // Enables test coverage
+      reporter: ['text', 'html'], // Generates coverage reports
+    },
+  },
+});
